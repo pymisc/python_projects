f6eba6e4266064ecc3e073bb21da1399f587ac33
vikas sharma <vs95054@gmail.com>
2024-09-26 22:50:53 -0700

    added palindrome program - v1

--- test002/001_palindrome.py
+++ test002/001_palindrome.py
@@ -1,13 +1,13 @@
 """ checking if the given word is a palindrome """
 
 def is_palindrome(string):
-  reversed_string = string[::-1]
-  print("REVERSED STRING IS: ", reversed_string)
-  return string == reversed_string
+    reversed_string = string[::-1]
+    print("REVERSED STRING IS: ", reversed_string)
+    return string == reversed_string
 
 # Testing above function now
-word = "mdam"
-if is_palindrome(word):
-  print(f"{word} is a palindrome")
+WORD = "mdam"
+if is_palindrome(WORD):
+    print(f"{WORD} is a palindrome")
 else:
-  print(f"{word} is NOT a palindrome")
+    print(f"{WORD} is NOT a palindrome")
