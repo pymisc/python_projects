a8032277705ba452567d1a9e9f1957b6b6a72201
vikas sharma <vs95054@gmail.com>
2024-10-01 22:15:27 -0700

    Code updated as of Tue Oct  1 10:15:27 PM PDT 2024

--- test001/p2.py
+++ test001/p2.py
@@ -6,5 +6,15 @@ def print_func1():
 
 
 print_func1()
-print("Hello " * 5)
-print_func1()
+#print("Hello " * 5)
+#print_func1()
+
+s1 = "this is a test string"
+
+# STRING METHODS:
+print(f"Capitalize: {s1.capitalize()}")
+print(f"Upper case: {s1.upper()}")
+print(f"Lower case: {s1.lower()}")
+# Casefold is kinda same as lower but also deals with unicode
+print(f"Casefold: {s1.casefold()}") # Casefold is kinda same as lower but also deals with unicode
+
