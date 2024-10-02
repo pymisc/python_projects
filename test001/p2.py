859f430876e35536f94b162ea5677f92631deeb7
vikas sharma <vs95054@gmail.com>
2024-10-01 21:49:36 -0700

    Code updated as of Tue Oct  1 09:49:36 PM PDT 2024

---
+++ test001/p2.py
@@ -0,0 +1,10 @@
+""" this is to test string operations """
+
+def print_func1():
+    """ to print some same headers """
+    print("***" * 15)
+
+
+print_func1()
+print("Hello " * 5)
+print_func1()
