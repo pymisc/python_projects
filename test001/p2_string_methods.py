4adfd784a97f1bc1173cc0df6ece4c0bd8761929
vikas sharma <vs95054@gmail.com>
2024-10-05 08:31:42 -0700

    Sat Oct  5 08:31:42 AM PDT 2024

--- test001/p2_string_methods.py
+++ test001/p2_string_methods.py
@@ -20,7 +20,7 @@ print(f"Upper case: {s1.upper()}")      # To UPPERCASE entire string
 print(f"Lower case: {s1.lower()}")      # To lowercase entire string
 print(f"Casefold: {s1.casefold()}")     # Casefold is same as lower but also deals with unicode
 print(f"Centralized: {s1.center(40)}")  # Centralize the text with a total of n number of spaces
-print(f"Count text: {s1.count(" ")}")        # Count the occurance of specific word etc
+print(f"Count text: {s1.count(' ')}")   # Count the occurance of specific word etc
 
 
 # REVERSING STRINGS
