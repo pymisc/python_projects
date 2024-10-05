bd95e73fc04589cd9dc942c01191895c0898bbc3
vikas sharma <vs95054@gmail.com>
2024-10-05 08:39:36 -0700

    Sat Oct  5 08:39:36 AM PDT 2024

--- test001/p2_string_methods.py
+++ test001/p2_string_methods.py
@@ -12,15 +12,15 @@ def print_reverse(x):
 # print("Hello " * 5)
 # print_func1()
 
-s1 = "this is a test string"
+S1 = "this is a test string"
 
 # STRING METHODS:
-print(f"Capitalize: {s1.capitalize()}") # To capitalize first letter of string
-print(f"Upper case: {s1.upper()}")      # To UPPERCASE entire string
-print(f"Lower case: {s1.lower()}")      # To lowercase entire string
-print(f"Casefold: {s1.casefold()}")     # Casefold is same as lower but also deals with unicode
-print(f"Centralized: {s1.center(40)}")  # Centralize the text with a total of n number of spaces
-print(f"Count text: {s1.count(' ')}")   # Count the occurance of specific word etc
+print(f"Capitalize: {S1.capitalize()}") # To capitalize first letter of string
+print(f"Upper case: {S1.upper()}")      # To UPPERCASE entire string
+print(f"Lower case: {S1.lower()}")      # To lowercase entire string
+print(f"Casefold: {S1.casefold()}")     # Casefold is same as lower but also deals with unicode
+print(f"Centralized: {S1.center(40)}")  # Centralize the text with a total of n number of spaces
+print(f"Count text: {S1.count(' ')}")   # Count the occurance of specific word etc
 
 
 # REVERSING STRINGS
