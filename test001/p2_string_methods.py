7a3ea56efccab9f5e838fe446d15874618582835
vikas sharma <vs95054@gmail.com>
2024-10-05 06:32:24 -0700

    Sat Oct  5 06:32:24 AM PDT 2024

--- test001/p2_string_methods.py
+++ test001/p2_string_methods.py
@@ -5,18 +5,16 @@ def print_func1():
     print("***" * 15)
 
 
-print_func1()
-#print("Hello " * 5)
-#print_func1()
+# print_func1()
+# print("Hello " * 5)
+# print_func1()
 
 s1 = "this is a test string"
 
 # STRING METHODS:
-print(f"Capitalize: {s1.capitalize()}")
-print(f"Upper case: {s1.upper()}")
-print(f"Lower case: {s1.lower()}")
-# Casefold is kinda same as lower but also deals with unicode
-print(f"Casefold: {s1.casefold()}") # Casefold is same as lower BUT also deals with unicode
-
-
-
+print(f"Capitalize: {s1.capitalize()}") # To capitalize first letter of string
+print(f"Upper case: {s1.upper()}")      # To UPPERCASE entire string
+print(f"Lower case: {s1.lower()}")      # To lowercase entire string
+print(f"Casefold: {s1.casefold()}")     # Casefold is same as lower but also deals with unicode
+print(f"Centralized: {s1.center(40)}")  # Centralize the text with a total of n number of spaces
+print(f"Count text: {s1.count(" ")}")        # Count the occurance of specific word etc
