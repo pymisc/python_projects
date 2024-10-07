6779df620ab41f43c22c494240420d442ca822e4
vikas sharma <vs95054@gmail.com>
2024-10-07 07:17:23 -0700

    New code as of Mon Oct  7 07:17:23 AM PDT 2024

---
+++ test002/p7_file_handling.py
@@ -0,0 +1,20 @@
+""" this program demonstrate file handling """
+
+# READONLY FILE 
+
+F1 = open('/tmp/file1', 'r') 
+
+#print(F1.read())
+
+print(F1.readline(), end="")
+print(F1.readline(), end="")
+
+# FILE WRITES (no printing on screen)
+F2 = open('/tmp/file2', 'w')
+F2.write("Something I wanted to write\n")
+F2.write("Something else I wanted to write\n")
+F2.close()
+
+# FILE READ (the one written in previous step)
+F3 = open('/tmp/file2', 'r')
+print(F3.read())
