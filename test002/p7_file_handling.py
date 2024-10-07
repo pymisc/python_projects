6c97ca5c38a9c6f42ec9896ea5fa8477330f79a5
vikas sharma <vs95054@gmail.com>
2024-10-07 07:21:38 -0700

    New code as of Mon Oct  7 07:21:38 AM PDT 2024

--- test002/p7_file_handling.py
+++ test002/p7_file_handling.py
@@ -10,11 +10,11 @@ print(F1.readline(), end="")
 print(F1.readline(), end="")
 
 # FILE WRITES (no printing on screen)
-F2 = open('/tmp/file2', 'w')
+F2 = open('/tmp/file2', 'w', encoding="utf-8")
 F2.write("Something I wanted to write\n")
 F2.write("Something else I wanted to write\n")
 F2.close()
 
 # FILE READ (the one written in previous step)
-F3 = open('/tmp/file2', 'r')
+F3 = open('/tmp/file2', 'r', encoding="utf=8")
 print(F3.read())
